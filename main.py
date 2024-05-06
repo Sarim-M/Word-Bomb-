c1ec3172598900946f4e0bb8a1a69065158c15db
Sarim-M <168831106+Sarim-M@users.noreply.github.com>
2024-05-06 08:38:51 -0400

    5/6 commit
    I combined code from my balloon flight and original "word bomb" project to outline the basics of my new word bomb. Had to fix some errors and began to create a display for the program.

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import enchant
 import random
 import pygame
+from bomb import Bomb
+from start import Start
 
+pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Arial', 20)
+my_starter_font = pygame.font.SysFont('Bebas Nueue', 40)
+pygame.display.set_caption("Word Bomb")
+b = Bomb
+s = Start
+
+size = (800, 600)
+screen = pygame.display.set_mode(size)
 
 def pick_word():
   words = []
@@ -24,4 +36,60 @@ def word_check(word):
 
 
 #messages to be blitten
-start_message =
+start_message = my_starter_font.render("Welcome to Word Bomb!", True, (255,255,255))
+
+#variables
+chosen_words = []
+hearts = 3
+
+#program switches
+run = True
+GameStart = True
+
+
+while run and GameStart:
+  x = 0
+  guessed_word_checker = []
+  word = []
+  correct_letters = 0
+  picked_word = pick_word()
+  print("Write a word that you haven't already used and includes:")
+  print(picked_word)
+  for letters in picked_word:
+    word.append(letters)
+  guessed_word = input("Enter your word here: ")
+  guessed_word = guessed_word.upper()
+  legible_word = word_check(guessed_word)
+  if legible_word == True:
+    for letters in guessed_word:
+      guessed_word_checker.append(letters)
+    for i in guessed_word_checker:
+      guessed_word_checker_list_length = len(guessed_word_checker)
+    try:
+      if i == word[x] and x <= guessed_word_checker_list_length:
+        correct_letters = correct_letters + 1
+      if correct_letters != len(word):
+       x = x + 1
+    except IndexError as e:
+     print("Loading....")
+    except Exception as e:
+     print("An error has occurred:", e)
+    if correct_letters >= len(word):
+      chosen_words.append(guessed_word)
+      score = score + 10
+      print(score)
+      print("Correct!")
+    else:
+     print("Thats not right!")
+     hearts = hearts - 1
+
+
+  else:
+   print("That's not right! ")
+   hearts = hearts - 1
+
+
+
+
+#BLIT
+
