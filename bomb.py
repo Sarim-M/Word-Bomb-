3e41d4dc12e3049e16affc996258912f44b4906f
Sarim-M <168831106+Sarim-M@users.noreply.github.com>
2024-06-10 08:44:52 -0400

    6/10
    Fixed up my indentations and graphics. Should be done with everything tommorow.

--- bomb.py
+++ bomb.py
@@ -7,7 +7,7 @@ class Bomb:
         self.y = y
         self.image = pygame.image.load("bombaroo.png")
         self.image_size = self.image.get_size()
-        scale_size = (self.image_size[0] * .3, self.image_size[1] * .3)
+        scale_size = (self.image_size[0] * .5, self.image_size[1] * .5)
         self.image = pygame.transform.scale(self.image, scale_size)
         self.image_size = self.image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
